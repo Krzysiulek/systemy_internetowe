49f8ac592d23b5cd51c79b5108c3c82eeddbec4c
Krzysztof Czarnecki <czarneckikrzysztof97@gmail.com>
2021-03-14 13:24:50 +0100

    lab_a2 - poprawka do cookiesow

--- lab_a2/src/main/java/server/handlers/CookiesHandler.java
+++ lab_a2/src/main/java/server/handlers/CookiesHandler.java
@@ -10,19 +10,20 @@ import java.util.stream.IntStream;
 
 public class CookiesHandler implements HttpHandler {
     private static final int COOKIE_LENGTH = 20;
+    private static final String SET_COOKIE = "Set-Cookie";
 
-    // todo randomizeCookies
     @Override
     public void handle(HttpExchange exchange) throws
                                               IOException {
-
-        String cookieText = getRandomString();
-        String response = "Cookie: " + cookieText;
+        String cookieWithEchoPath = String.format("randomCookie1=%s; Path=/echo; Domain=localhost", getRandomString());
+        String cookieWithoutPath = String.format("randomCookie2=%s", getRandomString());
+        String cookieWithInvalidDomain = String.format("randomCookie3=%s; Domain=google.com", getRandomString());
+        String response = String.format("Cookies: %n%s%n%s%n%s", cookieWithEchoPath, cookieWithoutPath, cookieWithInvalidDomain);
 
         exchange.getResponseHeaders().add("Content-Type", "text/html");
-        exchange.getResponseHeaders().add("Set-Cookie", "randomCookie1=ABC; Path=/echo; Domain=localhost");
-        exchange.getResponseHeaders().add("Set-Cookie", "randomCookie2=XYZ");
-        exchange.getResponseHeaders().add("Set-Cookie", "randomCookie3=POI; Domain=google.com");
+        exchange.getResponseHeaders().add(SET_COOKIE, cookieWithEchoPath);
+        exchange.getResponseHeaders().add(SET_COOKIE, cookieWithoutPath);
+        exchange.getResponseHeaders().add(SET_COOKIE, cookieWithInvalidDomain);
 
         exchange.sendResponseHeaders(200, response.getBytes().length);
         OutputStream os = exchange.getResponseBody();
